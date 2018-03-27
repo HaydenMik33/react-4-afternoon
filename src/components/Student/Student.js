60e7dab5782fe1869668de6b235c5904a0fc3693
Hayden <anhazzing33@gmail.com>
2018-03-27 16:28:45 -0500

    afternoon react-4

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
-
+import axios from "axios";
 export default class Student extends Component {
   constructor() {
-    super()
+    super();
+     this.state ={
+       studentInfo :{}
+     }
 
   }
-
+componentDidMount(){
+  return axios.get(`http://localhost:3005/students/${this.props.match.params.id}`)
+             .then(results=>{
+               console.log(results.data)
+               this.setState(
+                 {
+                   studentInfo : results.data
+                 });
+             });
+}
   render() {
+   const {studentInfo} = this.state;
     return (
       <div className="box">
-        <h1>Student</h1>
+        <h1>Student:</h1>
+        <h1>{studentInfo.first_name} {studentInfo.last_name}</h1>
+        <h4>Grade : {studentInfo.grade}</h4>
+        <h4>Email : {studentInfo.email}</h4>
       </div>
+
     )
   }
 }
